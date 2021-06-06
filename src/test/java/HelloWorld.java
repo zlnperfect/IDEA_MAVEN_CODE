f07e7d91ba2aac233e529d99311d09b9ccd8bfe9
zlnperfect <2439324877@qq.com>
2021-06-06 17:40:00 +0800

    a 提交代码到远程仓库中去

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -7,4 +7,8 @@ public class HelloWorld {
     public void test(){
         System.out.println("本地新增加的方法，我们需要将其提交到");
     }
+
+    public void test_a(){
+        System.out.println("开发人员a 提交了新的代码，并且提交到远程仓库种");
+    }
 }
