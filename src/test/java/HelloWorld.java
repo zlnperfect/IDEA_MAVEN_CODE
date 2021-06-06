401e822858ca5ad07adf0598ce6c44884207083e
zlnperfect <2439324877@qq.com>
2021-06-06 17:26:12 +0800

    第二次提交 ，HelloWorld类中新增了一个 test方法

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,4 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("嘻嘻");
     }
+
+
+    public void test(){
+        System.out.println("本地新增加的方法，我们需要将其提交到");
+    }
 }
